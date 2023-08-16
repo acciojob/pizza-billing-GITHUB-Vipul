3d2b292f8356450c05932b0f2b8947e161636da2
unknown <00vipul7@gmail.com>
2023-08-16 19:43:29 +0530

    Solution correction

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,26 +5,30 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-
-    private boolean topping;
-    private boolean extraCheese;
+    private boolean takeAway=false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
-        if(this.isVeg) this.setPrice(300);
+        if(isVeg) this.setPrice(300);
         else this.setPrice(400);
     }
 
+    public boolean isTakeAway() {
+        return takeAway;
+    }
 
+    public void setTakeAway(boolean takeAway) {
+        this.takeAway = takeAway;
+    }
 
     public void setPrice(int price) {
         this.price = price;
-        this.bill=""+price;
+        this.bill="Pizza Base Price:"+ price+"\n";;
     }
 
     public void setBill(String bill) {
-        this.bill = bill;
+        this.bill +=bill;
     }
 
     public int getPrice(){
@@ -33,25 +37,38 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-        this.bill= String.valueOf(Integer.parseInt(this.bill)+80);
 
-    }
+            this.setBill("Extra Cheese Added: 80\n");
+            this.price += 80;
+        }
+
+
 
     public void addExtraToppings(){
         // your code goes here
-        if(isVeg)
-        this.bill= String.valueOf(Integer.parseInt(this.bill)+70);
-        else
-            this.bill= String.valueOf(Integer.parseInt(this.bill)+120);
+        if(isVeg) {
+            this.setBill("Extra Topping Added: 70\n");
+            this.price += 70;
+
+        }
+        else  {
+            this.setBill("Extra Topping Added: 120\n");
+            this.price += 120;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
-        this.bill= String.valueOf(Integer.parseInt(this.bill)+20);
+        if(!takeAway) {
+            this.setBill("Carry Bag Added: 20\n");
+            this.price += 20;
+            this.setTakeAway(true);
+        }
+
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        return this.bill+ "Total Price: "+ price +"\n";
     }
 }
