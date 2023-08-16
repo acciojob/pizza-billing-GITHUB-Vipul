cb0d7b40f12fc294f0158c4bc1ad47a7c2ab338e
unknown <00vipul7@gmail.com>
2023-08-16 18:38:23 +0530

    solution

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,25 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private boolean topping;
+    private boolean extraCheese;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(this.isVeg) this.setPrice(300);
+        else this.setPrice(400);
+    }
+
+
+
+    public void setPrice(int price) {
+        this.price = price;
+        this.bill=""+price;
+    }
+
+    public void setBill(String bill) {
+        this.bill = bill;
     }
 
     public int getPrice(){
@@ -17,18 +33,25 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        this.bill= String.valueOf(Integer.parseInt(this.bill)+80);
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg)
+        this.bill= String.valueOf(Integer.parseInt(this.bill)+70);
+        else
+            this.bill= String.valueOf(Integer.parseInt(this.bill)+120);
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.bill= String.valueOf(Integer.parseInt(this.bill)+20);
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        return "Your bill is "+this.bill;
     }
 }
